ef1a7251a51f9b92a5cc69f48e4d8a555af38bac
Per <per.olzon@gmail.com>
2017-11-10 17:59:23 +0100

    Klarat delar av första uppgiften

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sympy as sy
-sy.init_printing(use_unicode=True)
+import numpy as np
+import matplotlib.pyplot as plt
+
+sy.init_printing()
 
 
 sigma=sy.symbols('sigma')
@@ -7,14 +10,32 @@ A=sy.Matrix([[(sigma+3),4],[-(9/4),sigma-3]])
 eigen=A.eigenvals()
 
 
-print(eigen)
 
-t = sy.symbols('t')
+print(A.eigenvects())
+print(A.inv())
+
+(t,C1,C2) = sy.symbols('t C1 C2')
 x, y = sy.symbols('x, y', function=True)
 
-eq=(sy.Eq(sy.Derivative(x(t),t),(sigma+3)*x(t)+4*y(t)),
-    sy.Eq(sy.Derivative(y(t),t),-(9/4)*x(t)+(sigma-3)*y(t)))
-print(eq.subs(x,1))
+eq1=sy.Eq(sy.Derivative(x(t),t),(sigma+3)*x(t)+4*y(t))
+eq2=sy.Eq(sy.Derivative(y(t),t),-(9/4)*x(t)+(sigma-3)*y(t))
 
-solution=sy.dsolve()
-print(solution)
+
+general_solution=sy.dsolve([eq1,eq2],[x(t),y(t)])
+#Specific Value
+#value = [general_solution[0].subs([(C2,0.1),(C1,0.1)]),general_solution[1].subs([(C2,0.1),(C1,0.1)])]
+
+
+
+#print(value)
+
+xlam = sy.lambdify((t,sigma,C1,C2), general_solution[0].rhs, modules='numpy')
+ylam = sy.lambdify((t,sigma,C1,C2), general_solution[1].rhs, modules='numpy')
+
+tspace=np.linspace(-1.5,1.5,1000)
+
+x_vals = xlam(tspace,1,1,1)
+y_vals = ylam(tspace,1,1,1)
+plt.plot(x_vals,y_vals)
+plt.show()
+#for i in range(-1,1):
