a55f6ba6212abd04d9607d99e193668e0b5ea9b4
Per <per.olzon@gmail.com>
2017-11-14 11:46:01 +0100

    Sturkturerat om uppgiften

--- main.py
+++ main.py
@@ -4,41 +4,59 @@ import matplotlib.pyplot as plt
 
 sy.init_printing()
 
-
-sigma=sy.symbols('sigma')
-A=sy.Matrix([[(sigma+3),4],[-(9/4),sigma-3]])
-eigen=A.eigenvals()
-
-
-print('Eigenvectors for system')
-sy.pprint(A.eigenvects())
-print('Inverse')
-sy.pprint(A.inv())
-sy.pprint(sy.simplify(A.inv()))
-
-inverse=sy.simplify(A.inv())
-(t,C1,C2) = sy.symbols('t C1 C2')
-x, y = sy.symbols('x, y', function=True)
-
-eq1=sy.Eq(sy.Derivative(x(t),t),(sigma+3)*x(t)+4*y(t))
-eq2=sy.Eq(sy.Derivative(y(t),t),-(9/4)*x(t)+(sigma-3)*y(t))
+def first_exercise(sigma_value):
+    sigma=sy.symbols('sigma')
+    A=sy.Matrix([[(sigma+3),4],[sy.Rational(-9,4),sigma-3]])
+    eigen=A.eigenvals()
 
 
-general_solution=sy.dsolve([eq1,eq2],[x(t),y(t)])
-#Specific Value
-#value = [general_solution[0].subs([(C2,0.1),(C1,0.1)]),general_solution[1].subs([(C2,0.1),(C1,0.1)])]
+    print('Eigenvectors for system')
+    sy.pprint(A.eigenvects())
+    print('Inverse')
+    #sy.pprint(A.inv()[0])
+    sy.pprint(sy.simplify(A.inv()))
+
+    inverse=sy.simplify(A.inv())
+    (t,C1,C2) = sy.symbols('t C1 C2')
+    x, y = sy.symbols('x, y', function=True)
+
+    eq1=sy.Eq(sy.Derivative(x(t),t),(sigma+3)*x(t)+4*y(t))
+    eq2=sy.Eq(sy.Derivative(y(t),t),-(9/4)*x(t)+(sigma-3)*y(t))
+
+
+    general_solution=sy.dsolve([eq1,eq2],[x(t),y(t)])
+    #Specific Value
+    #value = [general_solution[0].subs([(C2,0.1),(C1,0.1)]),general_solution[1].subs([(C2,0.1),(C1,0.1)])]
 
 
 
-#print(value)
+    #print(value)
 
-xlam = sy.lambdify((t,sigma,C1,C2), general_solution[0].rhs, modules='numpy')
-ylam = sy.lambdify((t,sigma,C1,C2), general_solution[1].rhs, modules='numpy')
+    xlam = sy.lambdify((t,sigma,C1,C2), general_solution[0].rhs, modules='numpy')
+    ylam = sy.lambdify((t,sigma,C1,C2), general_solution[1].rhs, modules='numpy')
 
-tspace=np.linspace(-1.5,1.5,1000)
+    tspace=np.linspace(-1.5,1.5,1000)
 
-x_vals = xlam(tspace,1,1,1)
-y_vals = ylam(tspace,1,1,1)
-plt.plot(x_vals,y_vals)
-#plt.show()
+    x_vals = xlam(tspace,sigma_value,1,1)
+    y_vals = ylam(tspace,sigma_value,1,1)
+    plt.plot(x_vals,y_vals)
+    #plt.show()
 
+def second_exercise():
+    sigma,c,d = sy.symbols('sigma,c,d')
+    A = sy.Matrix([[(sigma - c*d), d**2], [-c**2, sigma + c*d]])
+    eigen = A.eigenvals()
+    print('Eigenvectors for system')
+    sy.pprint(A.eigenvects())
+    print('Inverse')
+    # sy.pprint(A.inv()[0])
+    sy.pprint(sy.simplify(A.inv()))
+
+    values=A.subs(sigma,-1)
+    print(values)
+    sy.pprint(values)
+
+if __name__=='__main__':
+    #first_exercise(1)
+    print('\n')
+    second_exercise()
