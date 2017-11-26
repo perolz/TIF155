e240399e8562f33f23c2ee4defe6554c8c9966b8
Per <per.olzon@gmail.com>
2017-11-26 16:54:25 +0100

    Fjärde uppgiften

--- Problem set 2/Damped pendulum.py
+++ Problem set 2/Damped pendulum.py
@@ -6,37 +6,66 @@ from sympy.plotting import plot_parametric
 from mpl_toolkits.mplot3d import Axes3D
 import pickle
 
-(g,l,gamma,m)=sy.symbols('g l gamma m',positive=True,constant=True)
-theta,omega=sy.symbols('theta omega',function=True)
-t=sy.symbols('t')
-#A=sy.Matrix([[0,1],[sy.Rational(g,l)*sy.sin(),sigma-3]])
+sy.init_printing()
 
-eq1=sy.Eq(sy.Derivative(theta(t),t),omega(t))
-eq2=sy.Eq(sy.Derivative(omega(t),t),-g/l*sy.sin(theta(t))-gamma/m*omega(t))
+(g, l, gamma, m) = sy.symbols('g l gamma m', positive=True, constant=True)
+theta, omega = sy.symbols('theta omega', function=True)
+t = sy.symbols('t')
+# A=sy.Matrix([[0,1],[sy.Rational(g,l)*sy.sin(),sigma-3]])
 
-system=[eq1,eq2]
+eq1 = sy.Eq(sy.Derivative(theta(t), t), omega(t))
+eq2 = sy.Eq(sy.Derivative(omega(t), t), -g / l * sy.sin(theta(t)) - gamma / m * omega(t))
+
+system = [eq1, eq2]
+
+# print(sy.solve(system))
+(theta0, omega0, t0, sigma) = sy.symbols('theta0,omega0,t0,sigma', positive=True)
+tprim = sy.symbols('tprim')
+(x, y) = sy.symbols('x,y', function=True)
+
+# For assignment a-d
+# eq3 = sy.Eq(y(tprim), t0 / theta0 * omega0 * y(tprim))
+# eq4 = sy.Eq(-sy.sin(x(tprim)) - sigma * y(tprim),
+#             sy.simplify(t0 / omega0 * (-g / l * sy.sin(theta0 * x(tprim)) - gamma * omega0 / m * y(tprim))))
+#
+# substitutedSystem = [z.subs([(theta(t), theta0 * x(tprim)), (omega(t), omega0 * y(tprim)), (t, t0 * tprim)]) for z in
+#                      system]
+# sol = [eq3, eq4]
+# sol = [z.subs(theta0, 1) for z in sol]
+# sy.pprint(substitutedSystem)
+# newsol = sy.nonlinsolve(sol, t0, omega0, sigma)
+
+# Reduced system with x and y
+#eq5 = sy.Eq(sy.Derivative(x(tprim), tprim), y)
+#eq6 = sy.Eq(sy.Derivative(y(tprim), tprim), -sy.sin(x) - sigma * y(tprim))
+eq5 = sy.Eq(0, y)
+eq6 = sy.Eq(0, -sy.sin(x) - sigma * y)
+system2 = [eq5, eq6]
+
+sy.pprint(system2)
+
+w = 4
+Y, X = np.mgrid[-w:w:100j, -w:w:100j]
+U=Y
+V=-np.sin(X)-Y
+Z=np.array([[np.linalg.norm([U[i][j],V[i][j]]) for i in range(100)]for j in range(100)])
+speed = np.sqrt(U**2 + V**2)
+UN=U/speed
+VN=U/speed
 
 
-#print(sy.solve(system))
-(theta0,omega0,t0,sigma)=sy.symbols('theta0,omega0,t0,sigma',positive=True,function=True,constant=True)
-tprim=sy.symbols('tprim')
-(x,y)=sy.symbols('x,y',function=True)
+v=np.linspace(0, 2.0, 15, endpoint=True)
+fig = plt.figure()
 
+ax0 = fig.add_subplot(1,1,1)
+ax0.set_ylim(-2,2)
+cp = plt.contourf(Y, X, Z)
 
-eq3=sy.Eq(y(tprim),t0/theta0*omega0*y(tprim))
-eq4=sy.Eq(-sy.sin(x(tprim))-sigma*y(tprim),
-          sy.simplify(t0/omega0*(-g/l*sy.sin(theta0*x(tprim))-gamma*omega0/m*y(tprim))))
-
-substitutedSystem=[z.subs([(theta(t),theta0*x(tprim)),(omega(t),omega0*y(tprim)),(t,t0*tprim)]) for z in system]
-sol=[eq3,eq4]
-sol=[z.subs(theta0,1) for z in sol]
-#sy.pprint(substitutedSystem)
-newsol=sy.nonlinsolve(sol,t0,omega0,sigma)
-
-sy.pprint(newsol)
+cb = plt.colorbar(cp,ticks=v)
+plt.quiver(X, Y, UN, VN,color='Red')
+plt.show()
 
 
 
-
-
+#print(sy.solve(system2,[x,y],check=False))
 
